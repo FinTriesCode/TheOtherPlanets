1a591a46e07be6e27f735cf0a08d32461a130b2c
FinTriesCode <harryfindlay01@gmail.com>
2021-04-23 11:24:39 +0100

    Movement added, but does not work
    redo the  tutorials to ensure this is done for friday.

---
+++ TheOtherPlanets - Unity Mobile Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float forceStrength;
+
+    
+    public void MoveUp()
+    {
+        //get the rigidbody that we will be using for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.up * forceStrength);
+
+    }
+    public void MoveDown()
+    {
+        //get the rigidbody that we will be using for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.down * forceStrength);
+
+    }
+    public void MoveLeft()
+    {
+        //get the rigidbody that we will be using for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.left * forceStrength);
+
+    }
+    public void MoveRight()
+    {
+        //get the rigidbody that we will be using for movement
+        Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+        //Move in the correct direction with the set force strength
+        ourRigidbody.AddForce(Vector2.right * forceStrength);
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TheOtherPlanets - Unity Mobile Game/Assets/Scripts/TestButtonAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButtonAction : MonoBehaviour
+{
+
+    public void TestButton()
+    {
+        Debug.Log("Button has been pressed!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
